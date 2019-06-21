b1bfa1146466b325f51d5a48e18b26da3c7c9f95
Santa Claus <santa@northpole.com>
2019-06-21 15:04:03 +0300

    imported httpresponse

--- instagram/views.py
+++ instagram/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Image,Profile
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 
 # Create your views here.
 @login_required(login_url='/accounts/login/')
